98b88b482274194cf249881b10ec9fd4c4c78e1c
Alex T <52086722+tsss14@users.noreply.github.com>
2024-11-01 21:41:42 -0400

    added test test class

--- src/test/java/EdgeFieldTest.java
+++ src/test/java/EdgeFieldTest.java
@@ -1,1 +1,16 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
+public class EdgeFieldTest {
+
+    @Before
+    void setUp() {
+
+    }
+
+    @Test
+    void testTests() {
+        assertEquals("20 equals 20", 20, 20);
+    }
+}
