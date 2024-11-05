08598daaf666122ab6472b3a15ebc7ffbbc6be73
student <student@localhost.localdomain>
2024-11-04 19:15:11 -0500

    added some tests

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -14,20 +14,6 @@ public class CreateDDLMySQLTest{
 
     @Before
     public void setUp(){
-        /*
-        edgefld1 = new EdgeField("30|testField1");
-        edgefld2 = new EdgeField("28|testField2");
-        edgeflds[0] = edgefld1;
-        edgeflds[1] = edgefld2;
-
-        edgetbl1 = new EdgeTable("22|testTable1");
-        edgetbl2 = new EdgeTable("25|testTable2");
-        edgetbl1.setRelatedField(0,1);
-        edgetbl2.setRelatedField(1,1);
-        edgetbl1.makeArrays();
-        edgetbl2.makeArrays();
-        edgetbls[0] = edgetbl1;
-        edgetbls[1] = edgetbl2; */
         edgefld1 = new EdgeField("30|testField1");
         edgefld1.setDataType(0); 
         edgefld1.setVarcharValue(255);
@@ -53,54 +39,86 @@ public class CreateDDLMySQLTest{
         tester.databaseName = "MySQLDB";  
         
     }
+    @After
+    public void tearDown() {
+        edgefld1 = null;
+        edgefld2 = null;
+        edgetbl1 = null;
+        edgetbl2 = null;
+        edgeFields = null;
+        edgeTables = null;
+        tester = null;
+    }
+
     @Test
     public void testConstructor(){
-       
         assertNotNull("Tester should be initialized", tester);
+        assertNotNull("Tables should be initialized", tester.tables);
+        assertNotNull("Fields should be initialized", tester.fields);
     }
+
+    @Test
+    public void defaultConstructorTest() {
+        tester = new CreateDDLMySQL();
+        assertNull("Tables should be null for default constructor", tester.tables);
+        assertNull("Fields should be null for default constructor", tester.fields);
+    }
+
+    @Test
+    public void testCreateDDL() {
+        String sqlString = tester.getSQLString();
+        assertNotNull("SQL string should not be null", sqlString);
+        assertFalse("SQL string should not be empty", sqlString.isEmpty());
+        assertTrue("SQL string should contain USE statement", sqlString.contains("USE"));
+        assertTrue("SQL string should contain CREATE TABLE statement", sqlString.contains("CREATE TABLE"));
+        
+    }
+
     @Test
     public void convertBooleanFalse(){
-       
-
         String test = "false";
         assertEquals("Should be zero", 0, tester.convertStrBooleanToInt(test));
 
     }
+
     @Test
     public void convertBooleanTrue(){
-       
         String test = "true";
         assertEquals("Should be one", 1, tester.convertStrBooleanToInt(test));
     }
+
     @Test
     public void convertBooleanInvalid(){
-       
         String test = "fghkf";
         assertEquals("Should be false", 0, tester.convertStrBooleanToInt(test));
     }
+
     @Test
     public void generateDBNameValid(){
-       
-        //how to set db name??
         assertEquals("Should be MySQLDB", tester.getDatabaseName(), tester.generateDatabaseName());
     }
+
     @Test
     public void getDBNameValid(){
-       
-        //how to set db name??
         assertEquals("Should be MySQLDB", "MySQLDB", tester.getDatabaseName());
     }
+
     @Test
     public void getDBNameInvalid(){
-       
-        //how to set db name??
         assertNotEquals("Should be different names", "randomstr", tester.getDatabaseName());
     }
+
     @Test
     public void getProductNameTest(){
-       
-        //how to set db name??
         assertEquals("Should be MySQL", "MySQL", tester.getProductName());
     }
-   
+
+    @Test
+    public void testGetSQLString() {
+        String sqlString = tester.getSQLString();
+        assertNotNull("SQL string should not be null", sqlString);
+        assertFalse("SQL string should not be empty", sqlString.isEmpty());
+        assertTrue("SQL string should contain USE statement", sqlString.contains("USE"));
+        assertTrue("SQL string should contain CREATE TABLE statement", sqlString.contains("CREATE TABLE"));
+    }
 }
